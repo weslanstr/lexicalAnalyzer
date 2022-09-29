a2b1b6949200ddc4bcfee814a4c0c66d1f251632
wilancaster <49531813+wlanca2@users.noreply.github.com>
2022-09-29 10:13:07 -0400

    initialize project

---
+++ src/lexicalyzer.java
@@ -0,0 +1,49 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class lexicalyzer {
+    public static void main(String[] args){
+        //initialization
+        System.out.print("""
+                |
+                * Lexilyzer for COSC 455
+                * by Wesley Lancaster
+                * Submitted on 10/??/22""");
+        System.out.println("\n|\nplease enter directory location of .txt file.....");
+        System.out.print(".....path entered");
+
+        //TODO access file without scanner?
+        /*
+        File x = new File(name);
+        if(x.exists()) {
+            System.out.print("....."+x.getName() +  " exists!" + "\n");
+        }
+        else {
+            System.out.println("......file does not exist");
+        }
+        System.out.println("|");
+        */
+
+        //TODO main loop
+        try{
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //TODO next()
+        //make my own scanner to properly read between space AND lines.
+        class next{}
+
+        //TODO kind()
+        class kind{}
+
+        //TODO value()
+        class value{}
+
+        //TODO position()
+        class position{}
+
+    }
+}
